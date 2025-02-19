09d4426104b41867d0d8754140e3e3765c003472
daveflcc <143441096+daveflcc@users.noreply.github.com>
2025-02-18 23:51:05 -0500

    Update convert_C_to_F_01.py

--- convert_C_to_F_01.py
+++ convert_C_to_F_01.py
@@ -1,5 +1,5 @@
 # FILE NAME - convert_C_to_F_01.py
-
+# DRG - Rerun for points 2025-02-18-2351
 # NAME: Andrew Gellatly
 # DATE: 2/18/2025
 # BRIEF DESCRIPTION:  A program that converts from C to F in degrees.
